ab3a5b852c63296fa6e8ecad18ba7bdc02e03e22
FuturON JMD <futuronpreschooljammalamadugu@gmail.com>
2026-07-23 18:41:58 +0530

    Add parent info card for each student

---
+++ parent-info.js
@@ -0,0 +1,11 @@
+// Parent information for each student
+// UPDATE THESE WITH ACTUAL PARENT NAMES
+const parentInfo = {
+    "VEDANSHI": { parent: "Parent of Vedanshi", phone: "", relation: "Parent" },
+    "HEMANVITH": { parent: "Parent of Hemanvith", phone: "", relation: "Parent" },
+    "BHAVYESH": { parent: "Parent of Bhavyesh", phone: "", relation: "Parent" },
+    "ISRA": { parent: "Parent of Isra", phone: "", relation: "Parent" },
+    "AYRA": { parent: "Parent of Ayra", phone: "", relation: "Parent" },
+    "AARUSH": { parent: "Parent of Aarush", phone: "", relation: "Parent" },
+    "PARNIK": { parent: "Parent of Parnik", phone: "", relation: "Parent" }
+};
